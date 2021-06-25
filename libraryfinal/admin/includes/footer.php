8b1f7f842d2604eb5e21fded9a39d05223162b0d
PraveenKandarpa <86481743+PraveenKandarpa@users.noreply.github.com>
2021-06-25 20:38:38 +0530

    Add files via upload

---
+++ libraryfinal/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Library Management System |<a href="https://google.com/" target="_blank"  > Designed by : Batch C-12</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ libraryfinal/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                       Library Management System |<a href="https://google.com/" target="_blank"  > Developed by:Batch C12</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
